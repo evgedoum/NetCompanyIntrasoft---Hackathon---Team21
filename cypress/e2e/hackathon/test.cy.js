14fb6656a67113f5f22917357ad5ea2b77891b59
Filippos Tolias <filtol001@yahoo.gr>
2023-12-22 04:22:53 +0200

    Complete test 1 Now we also check the cart preview

--- cypress/e2e/hackathon/test.cy.js
+++ cypress/e2e/hackathon/test.cy.js
@@ -7,27 +7,40 @@ describe('example to-do app', () => {
       // Since we want to visit the same URL at the start of all our tests,
       // we include it in our beforeEach function so that it runs before each test
       cy.visit('https://rahulshettyacademy.com/seleniumPractise')
-      cy.wait(500);
+    //   cy.wait(500);
     })
 
     it('Add one product to cart', () => {
-        // We use the `cy.get()` command to get all elements that match the selector.
-        // Then, we use `should` to assert that there are two matched items,
-        // which are the two default items.
+
         cy.get('.product-name').contains('Brocolli - 1 Kg').siblings('.product-action').click({scrollBehavior: 'center'});
         cy.get(".added").should("contain", "ADDED").parent().siblings('.product-name').contains('Brocolli - 1 Kg');
         
-        let price;
+        let brocolli_price;
         cy.get('.product-name').contains('Brocolli - 1 Kg').siblings('.product-price').then((text) => {
-            price = text.text();
-        })
-        cy.log(price);
+            brocolli_price = Number(text.text());
+        });
         
         cy.get('.cart-info table tbody tr:nth-child(1) td:nth-child(3)',{scrollBehavior: 'top'}).should('have.text', '1');
         cy.get('.cart-info table tbody tr:nth-child(2) td:nth-child(3)',{scrollBehavior: 'top'}).then(function(cart_price){
-            cart_price = cart_price.text();
-            expect(price).to.equal(cart_price);
-        })
-      })
+            cart_price = Number(cart_price.text());
+            expect(brocolli_price).to.equal(cart_price);
+        });
+        
+        //click cart
+        cy.get('.cart-icon').click();
+        // check if cart preview is open
+        cy.get('.cart-preview');
+
+        
+        cy.get('.cart-preview .cart-items').contains('Brocolli - 1 Kg').siblings('.product-price').then(function(text){
+            const cart_brocolli_price = Number(text.text());
+            expect(cart_brocolli_price).to.equal(brocolli_price)
+        });
+        cy.get('.cart-preview .cart-items').contains('Brocolli - 1 Kg').parent().parent().children('.product-total').children('.quantity').contains('1 No.');
+        cy.get('.cart-preview .cart-items').contains('Brocolli - 1 Kg').parent().parent().children('.product-total').children('.amount').then(function(text){
+            const brocolli_total_price = Number(text.text());
+            expect(brocolli_total_price).to.equal(brocolli_price)
+        });
+      });  
   })
   
